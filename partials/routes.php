c414834bfecee97c2cb6f1609c237452d45da6f6
unknown <rinirestiani.18630215@gmail.com>
2022-02-05 23:46:28 +0800

    Menambahkan database dan pages

---
+++ partials/routes.php
@@ -0,0 +1,25 @@
+<?php 
+if(isset($_GET['page'])){
+	$page = $_GET['page'];
+	switch ($page) {
+		case '':
+		case 'home':
+		file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+			break;
+		case 'lokasiread':
+		file_exists('pages/admin/lokasiread.php') ? include
+			'pages/admin/lokasiread.php' : include "pages/404.php";
+			break;
+		case 'lokasicreate':
+		file_exists('pages/admin/lokasicreate.php') ? include 
+		'pages/admin/lokasicreate.php' : include "pages/404.php";
+		break;
+		
+		default:
+			include "pages/404.php";
+	}
+} else {
+	include "pages/home.php";
+}
+
+ ?>
